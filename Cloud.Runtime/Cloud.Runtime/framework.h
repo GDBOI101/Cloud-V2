b7294e46bd066fb4dcd79989521315de0b108e1a
GDBOI101 <61432333+GDBOI101@users.noreply.github.com>
2021-06-14 08:41:34 -0700

    Started WIP
    Coming Soon...

--- Cloud.Runtime/Cloud.Runtime/framework.h
+++ Cloud.Runtime/Cloud.Runtime/framework.h
@@ -4,8 +4,10 @@
  */
 #pragma once
 //Settings
-#define HOST std::string("127.0.0.1") //If hosting on Local Client it should be set to "127.0.0.1". 
-#define PORT std::string("4495") //If hosting on Local Client its the port your server is listening on.
+#define HOST std::string("127.0.0.1") //If hosting on Local Client it should be set to "127.0.0.1". If hosting on Cloud Hosted Backend change to you domain eg: cloudfn.dev
+#define PORT std::string("4495") //If hosting on Local Client its the port your server is listening on. If hosting on Cloud this does not matter
+//#define CLOUD //remove "//" at the beggining if you want host of of a Cloud Hosted Backend
+#define LOCAL //(Default) add "//" at the beggining if hosting on Cloud
 
 //includes
 #include <Windows.h>
