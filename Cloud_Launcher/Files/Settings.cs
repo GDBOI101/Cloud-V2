44b56f50d58dc7ba976e46e0ee63b005b00314f3
zinx-YT <63958372+zinx-YT@users.noreply.github.com>
2021-07-11 14:13:31 +0100

    Add files via upload

---
+++ Cloud_Launcher/Files/Settings.cs
@@ -0,0 +1,62 @@
+﻿
+using Newtonsoft.Json;
+using System.IO;
+using System.Reflection;
+
+using static System.Environment;
+// hey guess what this is aurora code // so uhh credits for pog work
+namespace Cloud_Launcher_v2
+{
+    class Configuration
+    {
+        string _path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Settings.json");
+
+        [JsonProperty("InstallLocation")]
+        public string InstallLocation { get; set; }
+        [JsonProperty("Email")]
+        public string Email { get; set; }
+        [JsonProperty("Password")]
+        public string Password { get; set; }
+
+
+        internal const string ClientExecutable = "FortniteClient-Win64-Shipping.exe";
+#if !NO_EGL
+        internal const string ClientArguments = "";
+#else
+        internal const string ClientArguments = "-epicapp=Fortnite -epicenv=Prod -epiclocale=en-us -epicportal";
+#endif
+
+        internal const string LauncherNative = "Cloud.Native.dll";
+        internal const string LauncherUri = "https://cloudfn-backend.herokuapp.com";
+
+        // TODO: Figure out how to generate FLToken's without hardcoding them.
+        internal const string BEToken = "f7b9gah4h5380d10f721dd6a";
+        internal const string EACToken = "10ga222d803bh65851660E3d";
+        public void Open()
+        {
+            if (File.Exists(_path))
+            {
+                var configuration = JsonConvert.DeserializeObject<Configuration>(File.ReadAllText(_path));
+                InstallLocation = configuration.InstallLocation;
+                Email = configuration.Email;
+                Password = configuration.Password;
+            }
+            else
+            {
+                foreach (EpicGames.Installed.Installation installation
+                    in EpicGames.LauncherInstalled.InstallationList)
+                {
+                    if (installation.AppName == "Fortnite") // finds fortnite
+                        InstallLocation = installation.InstallLocation;
+                }
+
+                
+
+                Save();
+            }
+        }
+
+
+        public void Save() => File.WriteAllText(_path, JsonConvert.SerializeObject(this)); // save
+    }
+}
---
+++ Cloud_Launcher/Form2.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cloud_Launcher_v2
+{
+    partial class Form2 : Form
+    {
+        class Installed
+        {
+
+
+            public class Installation
+            {
+                public string InstallLocation { get; set; }
+
+                public string AppName { get; set; }
+                public string AppVersion { get; set; }
+            }
+
+            public Installation[] InstallationList { get; set; }
+        }
+
+
+        static Process _clientProcess;
+        /// <summary>
+        /// 0 = None, 1 = BattlEye, 2 = EasyAntiCheat
+        /// </summary>
+        static byte _clientAnticheat;
+
+        public Configuration Configuration;
+        public Form2()
+        {
+            InitializeComponent();
+            Configuration = new Configuration();
+            Configuration.Open();
+
+            UserName.Text = Configuration.Email;
+            Password.Text = Configuration.Password;
+            FortnitePath.Text = Configuration.InstallLocation;
+        }
+
+        private void FortnitePath_TextChanged(object sender, EventArgs e)
+        {
+            Configuration.InstallLocation = FortnitePath.Text;
+            Configuration.Save();
+        }
+
+        private void Password_TextChanged(object sender, EventArgs e)
+        {
+            Configuration.Password = Password.Text;
+            Configuration.Save();
+        }
+
+        private void UserName_TextChanged(object sender, EventArgs e)
+        {
+            Configuration.Email = UserName.Text;
+            Configuration.Save();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            Password.UseSystemPasswordChar =
+                !Password.UseSystemPasswordChar;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Password.UseSystemPasswordChar =
+                !Password.UseSystemPasswordChar;
+
+            if (Password.UseSystemPasswordChar)
+                button2.Text = "Show";
+            else
+                button2.Text = "Hide";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialogBrowse.ShowDialog() == DialogResult.OK)
+                FortnitePath.Text = folderBrowserDialogBrowse.SelectedPath;
+        }
+    }
+}
