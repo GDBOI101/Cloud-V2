40f6196fb6c38f3eb4dcb7d891672324b2015771
zinx-YT <63958372+zinx-YT@users.noreply.github.com>
2021-07-30 17:48:29 +0000

    Add files via upload

---
+++ Cloud_Launcher/Cloud/stringvalues.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    public class stringvalues
+    {
+        string chapter1BE = "f7b9gah4h5380d10f721dd6a";
+        string chapter1EAC = "10ga222d803bh65851660E3d";
+        //string chapter2 = ""; // i will add chapter 2 later
+        internal const string ClientExecutable = "FortniteClient-Win64-Shipping.exe";
+        internal const string dll = "CloudC1.dll";
+    }
+
+}
